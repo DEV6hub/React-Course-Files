cb12af5f906ef9bad4a4f10bfe596a9102357d60
Rod Nolan <rodnolan@gmail.com>
2018-01-15 14:37:02 -0500

    refactor functional component to class-based component - remove function

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
 
-const NavBarFunction = props => {
-  let catalogCount = Object.keys(props.shirts).length;
-  let cartCount = Object.keys(props.cartItems).length;
-  return (
-    <nav className="navmenu">
-      <Link to="/">
-        <button>Catalog - {catalogCount} saved items</button>
-      </Link>
-      <Link to="/cart">
-        <button>Cart - {cartCount} items</button>
-      </Link>
-    </nav>
-  );
-};
-
 class NavBar extends Component {
   render(props) {
     let catalogCount = Object.keys(this.props.shirts).length;
