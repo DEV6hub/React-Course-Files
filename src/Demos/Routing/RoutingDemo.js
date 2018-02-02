67cf712b4cb08048d19f2159843b506cb316dce2
Rod Nolan <rodnolan@gmail.com>
2018-02-02 16:19:59 -0500

    sort this out later

--- src/Demos/Routing/RoutingDemo.js
+++ src/Demos/Routing/RoutingDemo.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default class RoutingDemo extends React.Component {
   render() {
-    let demo = 1;
+    let demo = 2;
     switch (demo) {
       case 1:
         return this.render1('Using the component and render functions');
@@ -27,26 +27,26 @@ export default class RoutingDemo extends React.Component {
     console.log('Demo::render1 ' + title);
 
     // 1) define and display a simple component with no props
-    let User = () => <p>Hello User</p>;
-    return <User />;
+    // let User = () => <p>Hello User</p>;
+    // return <User />;
 
     // 2) add props to the component and display them; no route involved
-    //let User = ({username}) => (<p>Hello {username}</p>);
-    //return (<User username="Bob"/>);
+    let User = ({ username }) => <p>Hello {username}</p>;
+    // return (<User username="Bob"/>);
 
     // 3) add route for /bob using incorrect syntax and show the error
     // return (<Router><div>
-    //     <Route path="/user" component={ <User username="Bob"/> } />
+    //     <Route path="/routing/user" component={ <User username="Bob"/> } />
     // </div></Router>);
 
     // 4) correct the syntax but lose the username prop
     // return (<Router><div>
-    //     <Route path="/user" component={ User } />
+    //     <Route path="/routing/user" component={ User } />
     // </div></Router>);
 
     // 5) define the function inline to regain the ability to pass props
     // return (<Router><div>
-    //     <Route path="/user" component={ () => (<User username="Bob" /> ) } />
+    //     <Route path="/routing/user" component={ () => (<User username="Barb" /> ) } />
     // </div></Router>);
 
     // 6) a) put username in state so that it will trigger a render...
@@ -55,16 +55,23 @@ export default class RoutingDemo extends React.Component {
     // return (<Router><div>
     //     {this.buttons()}
     //     <p>navigate to "/user" to get started</p>
-    //     <Route path="/user" component={ () => (<User username={this.state.username} /> ) } />
-    //     {/* <Route path="/user" component={ () => (<User2 username={this.state.username} /> ) } /> */}
+    //     {/* <Route path="/routing/user" component={ () => (<User username={this.state.username} /> ) } /> */}
+    //     {/* <Route path="/routing/user" component={ () => (<User2 username={this.state.username} /> ) } /> */}
     //     {/* if the component function is defined inline, React.createElement() is called everytime the function executes */}
     // </div></Router>);
 
     // 7) switch to the render prop to solve the unmount/mount problem
-    // return (<Router><div>
-    //     {this.buttons()}
-    //     <Route path="/user" render={ () => (<User2 username={this.state.username} /> ) } />
-    // </div></Router>);
+    return (
+      <Router>
+        <div>
+          {this.buttons()}
+          <Route
+            path="/routing/user"
+            render={() => <User2 username={this.state.username} />}
+          />
+        </div>
+      </Router>
+    );
 
     return null;
   }
@@ -111,6 +118,7 @@ export default class RoutingDemo extends React.Component {
   }
   // used in conjunction with render1()
 
+  getArticle = () => 'article';
   // display route props: match, location, history
   render2(title) {
     console.log('Demo::render2 ' + title);
@@ -119,15 +127,16 @@ export default class RoutingDemo extends React.Component {
     return (
       <Router>
         <div>
-          <Link to="/params">part 1</Link> {' | '}
-          <Link to="/params/bob">part 2</Link> {' | '}
-          <Link to="/params/bob/allparams">part 3</Link> {' | '}
-          <Link to="/params/bob/paramExperiments">part 4</Link>
+          <Link to="/routing/params">part 1</Link> {' | '}
+          <Link to="/routing/params/bob">part 2a</Link> {' | '}
+          <Link to="/routing/params/barb">part 2b</Link> {' | '}
+          <Link to="/routing/params/bob/allparams">part 3</Link> {' | '}
+          <Link to="/routing/params/bob/paramExperiments">part 4</Link>
           <br />
           {/* 1) start simple: simple path, simple render function, simple component */}
           <Route
             exact
-            path="/params"
+            path="/routing/params"
             render={() => (
               <div>
                 <User username="Bob" />
@@ -142,21 +151,29 @@ export default class RoutingDemo extends React.Component {
                     update the first Route to use exact to make it disappear
             */}
           <Route
-            exact
-            path="/params/:username"
-            render={({ match }) => (
-              <div>
-                <User username={match.params.username} />
-                <hr />
-              </div>
-            )}
+            path="/routing/params/:username"
+            render={({ match }) => {
+              let article = this.getArticle(match.params.username);
+              // if (article) {
+              //   return <Article data={ article }/>
+              // } else {
+              //   return <NotFound />
+              // }
+              return (
+                <div>
+                  {article ? <p>article</p> : <p>not found</p>}
+                  <User username={match.params.username} />
+                  <hr />
+                </div>
+              );
+            }}
           />
           {/* 
                 3) render function accepts all params and dumps them to the screen  
                    update the second route to use exact to make it disappear
             */}
           <Route
-            path="/params/:username/allparams"
+            path="/routing/params/:username/allparams"
             render={({ match, location, history }) => (
               <div>
                 <pre>
@@ -193,7 +210,7 @@ export default class RoutingDemo extends React.Component {
                     click the 
             */}
           <Route
-            path="/params/:username/paramExperiments"
+            path="/routing/params/:username/paramExperiments"
             render={({ match, location, history }) => (
               <div>
                 <div>
@@ -271,32 +288,52 @@ export default class RoutingDemo extends React.Component {
         <div>
           {/* 2) each component is associated with a specific route path */}
           <Links />
-          <Route path="/here" component={Here} />
-          <Route path="/there" component={There} />
+          {/* <Route path="/here" component={Here} />
+          <Route path="/there" component={There} /> */}
 
           {/* 4) use children() to deactivate links to the url you're already on */}
-          {/* <Links2 /> */}
+          <Links2 />
 
           {/* 3) the route below has a children property  */}
           {/* 3) the children function executes ALWAYS, not just when the path matches */}
           {/* 3) when the path doesn't match though, the match param is null */}
-          {/*             
-            <Route 
-                path="/here" 
-                children={ ({match, location}) => (<div style={{border: '1px solid black'}}>
-                    <p><code>Route.path</code>: /here.</p>
-                    <p><code>location.pathname</code>: {location.pathname}</p>
-                    {!match && <p><code>Route.path != location.pathname</code> so...</p>}
-                    {match && <p><code>Route.path == location.pathname</code> so...</p>}
-                    {
-                        !match 
-                        ? <p><code>match</code>: null</p>  
-                        : <p><code>match.path</code>: NOT null ({match.path})</p>
-                    }    
-                    <p>The <code>children</code> function always executes and returns <em>something</em>, even when <code>match</code> is null.</p>
-                </div>)}
-            />
-            */}
+
+          <Route
+            path="/here"
+            children={({ match, location }) => (
+              <div style={{ border: '1px solid black' }}>
+                <p>
+                  <code>Route.path</code>: /here.
+                </p>
+                <p>
+                  <code>location.pathname</code>: {location.pathname}
+                </p>
+                {!match && (
+                  <p>
+                    <code>Route.path != location.pathname</code> so...
+                  </p>
+                )}
+                {match && (
+                  <p>
+                    <code>Route.path == location.pathname</code> so...
+                  </p>
+                )}
+                {!match ? (
+                  <p>
+                    <code>match</code>: null
+                  </p>
+                ) : (
+                  <p>
+                    <code>match.path</code>: NOT null ({match.path})
+                  </p>
+                )}
+                <p>
+                  The <code>children</code> function always executes and returns{' '}
+                  <em>something</em>, even when <code>match</code> is null.
+                </p>
+              </div>
+            )}
+          />
 
           {/* 4) this is not the same thing 
             <div>This always displays too but that's because it isn't associated with a route. 
