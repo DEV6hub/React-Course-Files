6601119864a735437c01b730859420085fe414c3
Rod Nolan <rodnolan@gmail.com>
2018-02-12 15:15:53 -0500

    simplified main app component, stripped out NavBar  and Router/Routes

--- src/Demos/Demos.js
+++ src/Demos/Demos.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
 import RoutingDemo from './Routing/RoutingDemo';
 import FunWithLinks from './Routing/FunWithLinks';
 import StateDemo from './State/StateDemo';
@@ -9,43 +7,23 @@ import TypeCheckingDemo from './TypeChecking/TypeCheckingDemo';
 
 import './Demos.css';
 
-const Nav = () => (
-  <div style={styles.navmenu}>
-    <Link to="/routing">Routing</Link>
-    <Link to="/links">Links</Link>
-    <Link to="/state">State</Link>
-    <Link to="/typechecking">Type Checking</Link>
-  </div>
-);
-
 export default class Demos extends React.Component {
   render() {
-    console.log('App::render');
+    console.log('Demos::render');
     return (
-      <Router>
-        <div className="container">
-          <div className="appTitle">
-            <h3>the demos page</h3>
-          </div>
-          <Route component={Nav} />
-          <div className="mainPageBody">
-            <Route path="/routing" component={RoutingDemo} />
-            <Route path="/links" component={FunWithLinks} />
-            <Route path="/state" component={StateDemo} />
-            <Route path="/typechecking" component={TypeCheckingDemo} />
-          </div>
+      <div className="container">
+        <div className="appTitle">
+          <h3>the demos page</h3>
         </div>
-      </Router>
+        <div className="mainPageBody">
+          <RoutingDemo />
+          {/* 
+  <StateDemo />
+  <FunWithLinks />
+  <TypeCheckingDemo />
+*/}
+        </div>
+      </div>
     );
   }
 }
-
-const styles = {
-  navmenu: {
-    backgroundColor: 'linen',
-    border: '1px solid lightsteelblue',
-    padding: '5px',
-    display: 'flex',
-    justifyContent: 'space-around'
-  }
-};
