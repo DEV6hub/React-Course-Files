6a91e1a378c2a8e0c92c327174fa4063c9ac913b
Rod Nolan <rodnolan@gmail.com>
2018-02-12 15:09:00 -0500

    removed the main app... this is complex enough

--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-//render one or the other
-
-// import ShirtShop from './ShirtShopApp/App';
-// ReactDOM.render(<ShirtShop />, document.getElementById('root'));
-
 import Demos from './Demos/Demos';
 ReactDOM.render(<Demos />, document.getElementById('root'));
 
