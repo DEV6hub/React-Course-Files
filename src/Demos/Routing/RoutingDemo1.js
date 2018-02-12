bdaa5a46efb294f45da25259e17f6fcda000d0b7
Rod Nolan <rodnolan@gmail.com>
2018-02-12 16:20:55 -0500

    RoutingDemo1 is easier to follow

--- src/Demos/Routing/RoutingDemo1.js
+++ src/Demos/Routing/RoutingDemo1.js
@@ -1,61 +1,53 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+// demonstrate how the component and render functions work
 export default class RoutingDemo1 extends React.Component {
-  // demonstrate how the component and render functions work
   render() {
-    console.log('RoutingDemo1');
+    console.log('RoutingDemo1::render()');
 
-    // 1) define and display a simple component with no props
-    //let User = () => <p>Hello User</p>;
-    //return <User />;
+    let User;
+    User = () => <p>Hello User</p>;
+    //User = ({ username }) => <p>Hello {username}</p>;
 
-    // 2) add props to the component and display them; no route involved
-    let User = ({ username }) => <p>Hello {username}</p>;
-    //return (<User username="Bob"/>);
-
-    // 3) add route for /bob using incorrect syntax and show the error
-    // return (<Router><div>
-    //     <Route path="/routing/user" component={ <User username="Bob"/> } />
-    // </div></Router>);
-
-    // 4) correct the syntax but lose the username prop
-    // return (<Router><div>
-    //     <Route path="/routing/user" component={ User } />
-    // </div></Router>);
-
-    // 5) define the function inline to regain the ability to pass props
-    // return (<Router><div>
-    //     <Route path="/routing/user" component={ () => (<User username="Barb" /> ) } />
-    // </div></Router>);
-
-    // 6) a) put username in state so that it will trigger a render...
-    // 6) b) ...switch to User2 to show in the console how the component mounts and unmounts every time it's rendered.
-    //        hint: click the 'update something useless' button; the state change will trigger a render
-    // return (<Router><div>
-    //     {this.buttons()}
-    //     <p>navigate to "/routing/user" to get started</p>
-    //     {/* <Route path="/routing/user" component={ () => (<User username={this.state.username} /> ) } /> */}
-    //     {/* <Route path="/routing/user" component={ () => (<User2 username={this.state.username} /> ) } /> */}
-    //     {/* if the component function is defined inline, React.createElement() is called everytime the function executes */}
-    // </div></Router>);
-
-    // 7) switch to the render prop to solve the unmount/mount problem
     return (
       <Router>
         <div>
-          {this.buttons()}
-          <Route
-            path="/routing/user"
-            render={() => <User2 username={this.state.username} />}
-          />
+          {/* 1) define and display a simple component with no props */}
+          <User />
+
+          {/* 2) add props to the component and display them; no route involved */}
+          {/* <User username="Bob"/> */}
+
+          {/* MANUALLY navigate to /routing/user to continue with this demo */}
+
+          {/* 3) add route for /bob using incorrect syntax and show the error */}
+          {/* <Route path="/routing/user" component={ <User username="Bob"/> } /> */}
+
+          {/* 4) correct the syntax but lose the username prop */}
+          {/* <Route path="/routing/user" component={ User } /> */}
+
+          {/* 5) define the function inline to regain the ability to pass props */}
+          {/* <Route path="/routing/user" component={ () => (<User username="Barb" /> ) } /> */}
+
+          {/* used by steps 6 and 7 */}
+          {/* {this.buttons()} */}
+
+          {/* 6) a) put username in state so that it will trigger a render when it changes
+          6) b) ...switch to User2 to show in the console how the component mounts and unmounts every time it's rendered.
+             hint: click the 'update something useless' button; the state change will trigger a render */}
+          {/* <Route path="/routing/user" component={ () => (<User username={this.state.username} /> ) } /> */}
+          {/* <Route path="/routing/user" component={ () => (<User2 username={this.state.username} /> ) } /> */}
+          {/* if the component function is defined inline, React.createElement() is called everytime the function executes */}
+
+          {/* 7) switch to the render prop to solve the unmount/mount problem */}
+          {/* <Route path="/routing/user" render={() => <User2 username={this.state.username} />} /> */}
         </div>
       </Router>
     );
-
-    return null;
   }
 
+  // EVERYTHING below here is used in conjunction with steps 6 and 7
   constructor() {
     super();
     this.state = {
@@ -63,12 +55,15 @@ export default class RoutingDemo1 extends React.Component {
       useless: true
     };
   }
+
   updateUsername(un) {
     this.setState({ username: un });
   }
+
   updateUseless() {
     this.setState({ useless: !this.state.useless });
   }
+
   buttons() {
     return (
       <div>
@@ -98,7 +93,6 @@ export default class RoutingDemo1 extends React.Component {
   }
 }
 
-// used in conjunction with steps 6 and 7
 class User2 extends React.Component {
   componentDidMount() {
     console.log('User2::componentDidMount');
